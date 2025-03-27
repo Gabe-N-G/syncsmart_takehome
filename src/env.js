b09ce66408aa7fda93c610bddc652a7685fa00af
g g <gabedotgutierrez@gmail.com>
2025-03-27 12:16:51 -0400

    seeding data working partially

--- src/env.js
+++ src/env.js
@@ -18,7 +18,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_PARENTVAR: z.string()
+    // NEXT_PUBLIC_PARENTVAR: z.string()
   },
 
   /**
@@ -27,7 +27,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_PARENTVAR: process.env.PARENTVAR
+    // NEXT_PUBLIC_PARENTVAR: process.env.PARENTVAR
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,16 +1,30 @@
+'use server';
 
 import { faker } from "@faker-js/faker";
 import axios from "axios";
 import { env } from "~/env";
 // Function to generate and insert 100 fake users
 const contactURL = "https://api.hubapi.com/crm/v3/objects/contacts"
-
+const batchURL = "https://api.hubapi.com/crm/v3/objects/contacts/batch/create"
 const parentHeaders ={
   headers:{
-    Authorization: `Bearer ${env.NEXT_PUBLIC_PARENTVAR}`,
+    Authorization: `Bearer ${process.env.PARENTVAR}`,
     'Content-Type': 'application/json'
   }
 }
+
+const hubspot = require('@hubspot/api-client');
+
+const hubspotClient = new hubspot.Client({"accessToken":"YOUR_ACCESS_TOKEN"});
+
+const BatchInputSimplePublicObjectBatchInputUpsert = { inputs: [
+  {"idProperty":"string",
+    "objectWriteTraceId":"string",
+    "id":"string","properties":{"lastname":"S.","firstname":"Mark"}}
+  ] 
+};
+
+
 
 export default async function seedUsers() {
   // console.log("clicked!")
@@ -19,7 +33,7 @@ export default async function seedUsers() {
     firstname: string, 
     lastname: string }}[] = [];
 
-      for (let i = 0; i < 100; i++) {
+      for (let i = 0; i < 10; i++) {
         // Generate fake user data
         const firstName = faker.person.firstName();
         const lastName = faker.person.lastName();
@@ -35,8 +49,9 @@ export default async function seedUsers() {
         };
         inputs.push(obj)
       }
-      console.log(inputs)
-      axios.put(contactURL,inputs,parentHeaders)
+      console.log({inputs})
+
+      axios.post(batchURL,{inputs},parentHeaders)
         .then((res) => {
           console.log("Sucessfully seeded 100 items", res)
         })
