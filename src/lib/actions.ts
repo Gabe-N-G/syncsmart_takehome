0a5ebc71aed59fbb5996740f0abc3b51808e1de9
g g <gabedotgutierrez@gmail.com>
2025-03-27 11:00:04 -0400

    faker done, starting post request, CORS error

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,36 +1,44 @@
 
 import { faker } from "@faker-js/faker";
 import axios from "axios";
-
+import { env } from "~/env";
 // Function to generate and insert 100 fake users
 const contactURL = "https://api.hubapi.com/crm/v3/objects/contacts"
 
+const parentHeaders ={
+  headers:{
+    Authorization: `Bearer ${env.PARENTVAR}`,
+    'Content-Type': 'application/json'
+  }
+}
+
 export default async function seedUsers() {
-  console.log("clicked!")
+  // console.log("clicked!")
   const inputs: { properties: { 
     email: string, 
     firstname: string, 
-    lastname: string }}[] = []
-  ;
+    lastname: string }}[] = [];
+
       for (let i = 0; i < 100; i++) {
-        
         // Generate fake user data
         const firstName = faker.person.firstName();
-        const lastName = faker.name.lastName();
+        const lastName = faker.person.lastName();
         const email = faker.internet.email();
         // Log the inserted user for reference
-        console.log(`Inserted user ${i + 1}: ${firstName} ${lastName} (${email})`);
-
+        console.log(`Inserted user ${i + 1}: ${firstName} ${lastName} ${email}`);
         const obj = {
               properties: {
-                  email,
+                  email: email,
                   firstname: firstName,
                   lastname: lastName
                }
         };
-
         inputs.push(obj)
       }
       console.log(inputs)
-      console.log('Successfully inserted 100 users into the database');
+      axios.put(contactURL,inputs,parentHeaders)
+        .then((res) => {
+          console.log("Sucessfully seeded 100 items", res)
+        })
+        .catch((err) => console.error(err))
   }
