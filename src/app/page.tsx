b8d98ae0428886c1380d206a45a0c32c7445d14e
g g <gabedotgutierrez@gmail.com>
2025-03-31 12:58:20 -0400

    tabled and sroted items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,15 +33,29 @@ export default async function Home() {
             <Button variant="outlined" color="error" onClick={archiveParent}>Archive users **For testing purposes only**</Button>
             <p>{`Total Contacts: ${parentData.length}`} </p>
             <div>
-              {parentData.length > 0 ? 
-              sortedParent.map(user =>(
-                <div key={user.id}>
-                  <p>First name: {user.properties.firstname} Last Name: {user.properties.lastname} email: {user.properties.email}</p>
-                </div> 
-              ))
-              :
-              "Use the seed Button to seed 100 users!"}
-            </div>
+                {parentData.length > 0 ? (
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {sortedParent.map(user => (
+                        <tr key={user.id}>
+                          <td>{user.properties.firstname}</td>
+                          <td>{user.properties.lastname}</td>
+                          <td>{user.properties.email}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                ) : (
+                  "Use the seed Button to seed 100 users!"
+                )}
+              </div>
           </Box>
           <div
             className={styles.card}
@@ -53,16 +67,30 @@ export default async function Home() {
               <Button variant="outlined"  color="error" onClick={archiveChild}>Archive users **For testing purposes only**</Button>
               <p>{`Total Contacts: ${childData.length}`} </p>
               <div>
-              {childData.length > 0 ? 
-              sortedChild.map(user =>(
-                <div key={user.id}>
-                  <p>First name: {user.properties.firstname} Last Name: {user.properties.lastname} email: {user.properties.email}</p>
-                </div> 
-              ))
-              :
-              "Use the seed Button to seed 100 users!"}
-            </div>
+                {childData.length > 0 ? (
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {sortedChild.map(user => (
+                        <tr key={user.id}>
+                          <td>{user.properties.firstname}</td>
+                          <td>{user.properties.lastname}</td>
+                          <td>{user.properties.email}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                ) : (
+                  "Use the seed Button to seed 100 users!"
+                )}
               </div>
+          </div>
         </div>
       </div>
     </main>
